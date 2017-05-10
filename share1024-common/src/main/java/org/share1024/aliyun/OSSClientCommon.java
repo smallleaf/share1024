0882006fdc8266e3a4876aad2a3610c2f3afa9ba
smallleaf <578986218@qq.com>
2017-05-10 15:07:38 +0800

    ...

--- share1024-common/src/main/java/org/share1024/aliyun/OSSClientCommon.java
+++ share1024-common/src/main/java/org/share1024/aliyun/OSSClientCommon.java
@@ -12,9 +12,5 @@ public class OSSClientCommon {
 	/***
 	 * buget名称
 	 */
-	public static final String BUCKET_SHARE="share1024";
-	public static final String OBJECT_SHARE_IMAGE_BISHE="images/bishe/";
-	public static final String END_POINT="http://oss-cn-qingdao.aliyuncs.com";
-	public static final String ACCESS_KEY_ID="LTAIoazhqHMG3qGI";
-	public static final String SECRET_ACCESS_KEY="hWryeelTCq8F24NbmkErxd1uBCOAgB";
+
 }
